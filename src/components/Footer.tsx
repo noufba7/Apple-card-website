9273c4f2f9ba4ad6478bb7c1a5eab1a50e40e0d5
Nouf Baabbad <nouf.baabad7@gmail.com>
2025-11-13 12:51:10 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,7 @@ const Footer = () => {
     <footer className="py-12 px-6 bg-black border-t border-white/10">
       <div className="max-w-6xl mx-auto text-center space-y-6">
         <div className="flex justify-center">
-          <img 
-            src={academyLogo} 
-            alt="Tuwaiq Academy & Apple Developer Academy" 
-            className="h-16 md:h-20 object-contain opacity-90"
-          />
+         
         </div>
       </div>
     </footer>
